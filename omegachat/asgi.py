c6b54bed5518b93d10e658c3f5a19ec1e773189c
aromalbabus404 <aromalbabus404@gmail.com>
2026-04-29 18:46:58 +0530

    intial commit

--- omegachat/asgi.py
+++ omegachat/asgi.py
@@ -2,7 +2,7 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
-from app.consumers import ChatConsumer
+from chat.consumers import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omegachat.settings')
 
